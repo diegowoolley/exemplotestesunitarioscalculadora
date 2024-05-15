365c3f44ef6c31e396fbc497be6ff99d2ee5c468
diegowoolley <154614603+diegowoolley@users.noreply.github.com>
2024-05-15 14:00:37 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using Calculadora;
+class Program
+{
+    static void Main(string[] args)
+    {
+        calculadora calc = new calculadora();
+
+        Console.WriteLine("Digite dois números:");
+        int n1 = Convert.ToInt32(Console.ReadLine());
+        int n2 = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Escolha uma operação:");
+        Console.WriteLine("1. Somar");
+        Console.WriteLine("2. Subtrair");
+        Console.WriteLine("3. Multiplicar");
+        Console.WriteLine("4. Dividir");
+        int opcao = Convert.ToInt32(Console.ReadLine());
+
+        int resultado = 0;
+        switch (opcao)
+        {
+            case 1:
+                resultado = calc.Somar(n1, n2);
+                break;
+            case 2:
+                resultado = calc.Subtrair(n1, n2);
+                break;
+            case 3:
+                resultado = calc.Multiplicar(n1, n2);
+                break;
+            case 4:
+                resultado = calc.Dividir(n1, n2);
+                break;
+            default:
+                Console.WriteLine("Opção inválida.");
+                break;
+        }
+
+        Console.WriteLine("Resultado: " + resultado);
+    }
+}
+
+
---
+++ calculadora.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculadora
+{
+    public class calculadora
+    {
+        public int Somar(int n1, int n2)
+        {
+            return n1 + n2;
+        }
+
+        public int Subtrair(int n1, int n2)
+        {
+            return n1 - n2;
+        }
+
+        public int Multiplicar(int n1, int n2)
+        {
+            return n1 * n2;
+        }
+
+        public int Dividir(int n1, int n2)
+        {
+            if (n2 != 0)
+            {
+                return n1 / n2;
+            }
+            else
+            {
+                throw new ArgumentException("Divisor não pode ser zero.");
+            }
+        }
+    }
+}
